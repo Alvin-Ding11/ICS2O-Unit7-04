2c1be42f71b8fd7d38e183b80aee75a2c564b73b
AlvinDing11 <alvin.ding@ocsbstudent.ca>
2021-03-24 14:40:14 +0000

    js fixes

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function fees () {
 
   day = document.getElementById('day').value
 
-  if ((day == 'Tuesday' || day == 'Thursday') || (age >= 12 && age <=21)) {
+  if ((day === 'Tuesday' || day === 'Thursday') || (age >= 12 && age <= 21)) {
     document.getElementById('line1').innerHTML = 'You are eligible for student pricing'
   } else {
     document.getElementById('line1').innerHTML = 'You must pay the regular price'
